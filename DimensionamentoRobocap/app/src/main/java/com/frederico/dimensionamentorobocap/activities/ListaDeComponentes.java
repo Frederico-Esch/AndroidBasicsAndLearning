d49538d9b45698b84449ea7b0bbcd72615be3d8f
Frederico Esch <fredeschpe@gmail.com>
2021-08-27 22:14:52 -0300

    flutter and robocap

---
+++ DimensionamentoRobocap/app/src/main/java/com/frederico/dimensionamentorobocap/activities/ListaDeComponentes.java
@@ -0,0 +1,14 @@
+package com.frederico.dimensionamentorobocap.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaDeComponentes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_de_componentes);
+    }
+}
